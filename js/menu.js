4144c5f91bba7df5d3d8795a99ab86e70151a48f
Raiady Valentine <Valentineraiady@gmail.com>
2023-09-23 21:25:48 -0400

    new up

---
+++ js/menu.js
@@ -0,0 +1,183 @@
+let foodJson = [
+    {
+        id: 1,
+        name: 'Combo X ',
+        img: 'images/Menu-1.svg',
+        price: [20.00, 33.00, 45.00],
+        sizes: [
+          'Solteiro',
+          'Casal',
+          'Familia'
+        ],
+        description:'Pão com gergelim,mussarela,tomate,alface,hambúrguer natural e picles'
+      },
+      {
+        id: 2,
+        name: 'Cachorro-quente de Carne ',
+        img: 'images/Menu-2.svg',
+        price: [21.00, 24.00, 26.00],
+        sizes: [
+          'Simples',
+          'Duplo',
+          'Moda da casa'
+        ],
+        description: 'Pão, cheddar, calabresa e carne fatiada, cebola ,pimentão e batata palha'
+      },
+      {
+        id: 3,
+        name: 'Bauru',
+        img: 'images/bauru.svg',
+        price: [8.00, 10.00, 9.00],
+        sizes: [
+          'Simples',
+          'Duplo',
+          'Moda da casa'
+        ],
+        description: 'Pão, cheddar,presunte e tomate'
+      },
+      {
+        id: 4,
+        name: 'Combo-X Frango-empanado',
+        img: 'images/Menu-4.svg',
+        price: [19.00, 28.00, 35.00],
+        sizes: [
+          'Solteiro',
+          'Casal',
+          'Familia'
+        ],
+        description: 'Pão,cebola,tomate,alface,Frango-empanado'
+      },
+      {
+        id: 5,
+        name: 'Porção de Coxinha',
+        img: 'images/Menu-5.svg',
+        price: [24.00, 27.00, 29.00],
+        sizes: [
+          'Porção 6 ',
+          'porção 12',
+          'porção 20'
+        ],
+        description: 'massa de trigo e frango desfiado'
+      },
+      {
+        id: 6,
+        name: 'X-bacon supreme',
+        img: 'images/Menu-6.svg',
+        price: [25.00, 33.00, 45.00],
+        sizes: [
+          'Simples',
+          'Duplo',
+          'Tiplo'
+        ],
+        description: 'Pão com gergelim,cheddar,cebola empanada,bacon e picles'
+      },
+      {
+        id: 7,
+        name: 'Porção de Nuggets com batata',
+        img: 'images/Menu-7.svg',
+        price: [27.00, 30.00, 42.00],
+        sizes: [
+          'Solteiro',
+          'Casal',
+          'Familia'
+        ],
+        description: 'Nugget de frango com batata frita e molho'
+      },
+      {
+        id: 8,
+        name: 'Sanduiche',
+        img: 'images/fast.svg',
+        price: [10.00, 22.00, 35.00],
+        sizes: [
+          'simples',
+          '15 Cm',
+          '30 Cm'
+        ],
+        description: 'Pão,Alface,tomate,cenora e recheio da sua escolha'
+      },
+
+      {
+        id: 9,
+        name: 'Porção de Pastel',
+        img: 'images/pastel.svg',
+        price: [24.00, 36.00, 40.00],
+        sizes: [
+          'Porção 4 ',
+          'porção 6',
+          'porção 10'
+        ],
+        description: 'massa de pastel e recheio a sua escolha'
+      },
+      {
+        id: 10,
+        name: 'Milk-shake',
+        img: 'images/milk.svg',
+        price: [13.00, 16.00, 10.00],
+        sizes: [
+          'Chocolate 400 ml',
+          'Morango 400 ml',
+          'Creme 400 ml'
+        ],
+        description: 'sorvete e leite'
+      },
+      {
+        id: 11,
+        name: 'Coca-cola',
+        img: 'images/coca.svg',
+        price: [5.00, 8.00, 12.00],
+        sizes: [
+          'lata',
+          '1 litro',
+          '2 Litros'
+        ],
+        description: 'Coca-cola'
+      },
+      {
+        id: 12,
+        name: 'Suco de Laranja',
+        img: 'images/laranja.svg',
+        price: [24.00, 27.00, 29.00],
+        sizes: [
+          '250 ml',
+          '500 ml',
+          'jarra'
+        ],
+        description: 'Laranja'
+      },
+      {
+        id: 13,
+        name: 'Agua',
+        img: 'images/Agua.svg',
+        price: [30.00, 33.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Agua Mineral'
+      },
+      {
+        id: 14,
+        name: 'Chopp',
+        img: 'images/garrafa.svg',
+        price: [27.00, 30.00, 32.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Chopp'
+      },
+      {
+        id: 15,
+        name: 'Soda Colorida',
+        img: 'images/soda.svg',
+        price: [30.00, 32.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Soda sabor a sua escolha'
+      }
+];
